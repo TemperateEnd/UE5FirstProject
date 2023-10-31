5ba89e96f2643a12591572d2c6e02d4627946337
ReeceTarrant <rtarrant70998@gmail.com>
2023-10-31 20:16:26 +0000

    First commit

---
+++ UE5FirstProject/Source/UE5FirstProject/UE5FirstProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5FirstProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5FirstProject, "UE5FirstProject" );
